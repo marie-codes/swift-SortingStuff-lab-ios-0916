c602a18b9b0c80f7a71d12e6ec801270905a0f68
mariejinpark <mariejinpark@gmail.com>
2016-10-31 21:33:46 -0400

    SortingStuff Lab - don't understand changeColor

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -17,10 +17,44 @@ struct ToyBin {
     var musicCDs: [MusicCD] = []
     
     // TODO: Implement all of the sort functions (lets organize this toy bin!)
-    
- 
-}
+    mutating func sortShips() {
+        ships.sorted(by: { (ship1, ship2) -> Bool in
+            var emptyShips = [Ship]()
+            for (index,ship) in ships.enumerated() {
+                if ship.name == "Titanic" {
+                    emptyShips.append(ship)
+                    ships.remove(at: index)
+                }
+            }
+            if ship1.age > ship2.age { return true } else { return false }
+        })
+    }
 
+    mutating func sortBooks() {
+        books.sorted(by: { $0.name < $1.name })
+    }
+
+    mutating func sortBowlingPins() {
+        bowlingPins.sorted(by:{ $0.color.rawValue < $1.color.rawValue })
+    }
+
+    mutating func sortMusicCDs() {
+         musicCDs = musicCDs.sorted(by: { $0.name < $1.name })
+            for (index, value) in musicCDs.enumerated() {
+                if value.name == "Drake" {
+                    let drake = musicCDs.remove(at: index); musicCDs.insert(drake, at: 0)
+                }
+            }
+    }
+
+    mutating func changeColorOfAllPins(to color:Color) {
+        bowlingPins = bowlingPins.map({ (pin) -> BowlingPin in
+                        let newPin = BowlingPin(name: pin.name, film: pin.film, color: color)
+                        return newPin
+                    })
+            }
+    
+}
 
 struct Ship {
     var name: String
@@ -58,3 +92,6 @@ struct MusicCD {
     var year: Int
     var songs: [String]
 }
+
+
+    
--- ClosuresAreGreat/ViewController.swift
+++ ClosuresAreGreat/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-  
-    
+   
 
     
 }
